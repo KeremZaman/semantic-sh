3b03923df5d216b125882c4cbab9e76c3252ed85
KeremZaman <kerem.zaman@boun.edu.tr>
2020-06-25 20:39:23 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='semantic_sh',
+    version='1.0',
+    packages=['semantic_sh'],
+    url='www.github.com/KeremZaman/semantic_sh',
+    license='MIT',
+    author='Kerem Zaman',
+    author_email='',
+    description='semantic-sh is a SimHash implementation to detect and group similar texts by taking power of word vectors and transformer-based language models (BERT).'
+)
