00e1bea24a9093bbae13a4a01cc7732deb9f2ddd
KeremZaman <kerem.zaman@boun.edu.tr>
2020-09-19 23:28:58 +0300

    Update version and description

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='semantic_sh',
-    version='1.3',
+    version='1.4',
     packages=['semantic_sh'],
     license='MIT',
     author='Kerem Zaman',
     author_email='',
-    description='semantic-sh is a SimHash implementation  to detect and group similar texts by taking power of word vectors and transformer-based language models (BERT).'
+    description='semantic-sh is a SimHash implementation to detect and group similar texts by taking power of word vectors and transformer-based language models such as BERT.'
 )
